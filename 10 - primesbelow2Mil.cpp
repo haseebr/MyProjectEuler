3a5a0ca337da083f8062936cbbb243f34e3f54d7
Haseeb ur Rahman <me@haseeb.co>
2014-10-24 19:09:21 +0530

    Update 10 - primesbelow2Mil.cpp

--- 10 - primesbelow2Mil.cpp
+++ 10 - primesbelow2Mil.cpp
@@ -1,19 +1,18 @@
 #include <iostream>
 #include <math.h>
 using namespace std;
-int main()
-{
+int main() {
     unsigned long long i,j,sum=0;
     int count1=0;
-    for(i=2;i<2000000;++i)
-    {
-        for(j=2;j<=sqrt(i);++j)
-        {
+    for(i=2;i<2000000;++i) {
+        for(j=2;j<=sqrt(i);++j) {
             if((i%j)==0)
             ++count1;
         }
-
-        if(count1==0){cout << i << endl;sum+=i;}
+        if(count1==0) {
+            cout << i << endl;
+            sum+=i;
+        }
         count1=0;
     }
     cout << sum;
